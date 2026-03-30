63f2d6c312b88418d5754a6c527806a2cb027d44
Raczyx <1596700738@qq.com>
2026-03-30 22:36:03 +0800

    update

--- exercises/03_os_concurrency/02_atomic_ordering/src/lib.rs
+++ exercises/03_os_concurrency/02_atomic_ordering/src/lib.rs
@@ -40,7 +40,8 @@ impl FlagChannel {
     pub fn produce(&self, value: u32) {
         // TODO: Store data (choose appropriate Ordering)
         // TODO: Set ready = true (choose appropriate Ordering so data writes complete before this)
-        todo!()
+        self.data.store(value, Ordering::Release);
+        self.ready.store(true, Ordering::Release);
     }
 
     /// Consumer: spin-wait for ready flag, then read data.
@@ -51,7 +52,10 @@ impl FlagChannel {
     pub fn consume(&self) -> u32 {
         // TODO: Spin-wait for ready to become true (choose appropriate Ordering)
         // TODO: Read data (choose appropriate Ordering)
-        todo!()
+        while !self.ready.load(Ordering::Acquire) {
+            
+        }
+        return self.data.load(Ordering::Acquire);
     }
 
     /// Reset channel state
@@ -83,13 +87,23 @@ impl OnceCell {
     pub fn init(&self, val: u32) -> bool {
         // TODO: Use compare_exchange to ensure initialization only once
         // Store value on success
-        todo!()
+        if self.initialized.load(Ordering::Acquire) {
+            return false;
+        }else{
+            self.value.store(val, Ordering::Release);
+            self.initialized.store(true, Ordering::Release);
+            return true;
+        }
     }
 
     /// Get value. Returns Some if initialized, otherwise None.
     pub fn get(&self) -> Option<u32> {
         // TODO: Check initialized flag, then read value
-        todo!()
+        if self.initialized.load(Ordering::Acquire) {
+            Some(self.value.load(Ordering::Acquire))
+        }else{
+            None
+        }
     }
 }
 
