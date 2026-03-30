b0c81ba1ad69bfef1c3c1ae0e95314338ab3bc80
Raczyx <1596700738@qq.com>
2026-03-30 22:42:38 +0800

    update

--- exercises/03_os_concurrency/04_spinlock_guard/src/lib.rs
+++ exercises/03_os_concurrency/04_spinlock_guard/src/lib.rs
@@ -10,6 +10,7 @@
 //! - Why manual lock/unlock is unsafe (forgetting unlock, panic without release)
 
 use std::cell::UnsafeCell;
+use core::hint;
 use std::ops::{Deref, DerefMut};
 use std::sync::atomic::{AtomicBool, Ordering};
 
@@ -41,7 +42,10 @@ impl<T> SpinLock<T> {
     pub fn lock(&self) -> SpinGuard<'_, T> {
         // TODO: Spin-wait to acquire lock
         // TODO: Return SpinGuard { lock: self }
-        todo!()
+        while let Err(_) = self.locked.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed) {
+            hint::spin_loop();
+        }
+        SpinGuard { lock: self }
     }
 }
 
@@ -51,7 +55,7 @@ impl<T> Deref for SpinGuard<'_, T> {
     type Target = T;
 
     fn deref(&self) -> &T {
-        todo!()
+        unsafe { & *self.lock.data.get() }
     }
 }
 
@@ -59,7 +63,7 @@ impl<T> Deref for SpinGuard<'_, T> {
 // Return &mut T
 impl<T> DerefMut for SpinGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut T {
-        todo!()
+        unsafe {&mut *self.lock.data.get() }
     }
 }
 
@@ -67,7 +71,7 @@ impl<T> DerefMut for SpinGuard<'_, T> {
 // Set lock.locked to false (Release ordering)
 impl<T> Drop for SpinGuard<'_, T> {
     fn drop(&mut self) {
-        todo!()
+        self.lock.locked.store(false, Ordering::Release);
     }
 }
 
