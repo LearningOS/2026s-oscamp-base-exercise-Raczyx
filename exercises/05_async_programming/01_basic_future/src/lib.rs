11af104d16931229b6abbbde69d1171d600ccbf6
Raczyx <1596700738@qq.com>
2026-04-13 22:40:42 +0800

    update

--- exercises/05_async_programming/01_basic_future/src/lib.rs
+++ exercises/05_async_programming/01_basic_future/src/lib.rs
@@ -33,7 +33,13 @@ impl Future for CountDown {
     type Output = &'static str;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        todo!()
+        if self.count == 0 {
+            return Poll::Ready("liftoff!");
+        }
+        self.get_mut().count -= 1;
+        cx.waker().wake_by_ref();
+
+        Poll::Pending
     }
 }
 
@@ -57,7 +63,14 @@ impl Future for YieldOnce {
     type Output = ();
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        todo!()
+        if self.yielded {
+            return Poll::Ready(());
+        }else{
+            self.get_mut().yielded = true;
+            cx.waker().clone().wake();
+            
+            return Poll::Pending;
+        }
     }
 }
 
